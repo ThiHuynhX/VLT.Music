911575f388a50cb7401361193d938ff6eaa412e5
Huynh Thi <thihuynh@gmail.com>
2024-05-14 19:14:41 +0700

    Add project files.

---
+++ VLT.Music/Vlt.Music.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VLT.Music
+{
+    partial class VltMusic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VltMusic));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmdLoad = new System.Windows.Forms.Button();
+            this.cmdPlay = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblPlayingSong = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(650, 213);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cmdLoad
+            // 
+            this.cmdLoad.Location = new System.Drawing.Point(12, 12);
+            this.cmdLoad.Name = "cmdLoad";
+            this.cmdLoad.Size = new System.Drawing.Size(75, 23);
+            this.cmdLoad.TabIndex = 1;
+            this.cmdLoad.Text = "Load List";
+            this.cmdLoad.UseVisualStyleBackColor = true;
+            this.cmdLoad.Click += new System.EventHandler(this.cmdLoad_Click);
+            // 
+            // cmdPlay
+            // 
+            this.cmdPlay.Location = new System.Drawing.Point(587, 12);
+            this.cmdPlay.Name = "cmdPlay";
+            this.cmdPlay.Size = new System.Drawing.Size(75, 23);
+            this.cmdPlay.TabIndex = 1;
+            this.cmdPlay.Text = "Play";
+            this.cmdPlay.UseVisualStyleBackColor = true;
+            this.cmdPlay.Click += new System.EventHandler(this.cmdPlay_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "I:\\TMP";
+            // 
+            // lblPlayingSong
+            // 
+            this.lblPlayingSong.AutoSize = true;
+            this.lblPlayingSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayingSong.Location = new System.Drawing.Point(152, 17);
+            this.lblPlayingSong.Name = "lblPlayingSong";
+            this.lblPlayingSong.Size = new System.Drawing.Size(47, 15);
+            this.lblPlayingSong.TabIndex = 2;
+            this.lblPlayingSong.Text = "label1";
+            // 
+            // VltMusic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 269);
+            this.Controls.Add(this.lblPlayingSong);
+            this.Controls.Add(this.cmdPlay);
+            this.Controls.Add(this.cmdLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VltMusic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vietluxtour Music";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cmdLoad;
+        private System.Windows.Forms.Button cmdPlay;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblPlayingSong;
+    }
+}
+
---
+++ VLT.Music/Vlt.Music.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VLT.Music
+{
+    public partial class VltMusic : Form
+    {
+
+        List<string> lstFiles = new List<string>();
+        public VltMusic()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdLoad_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {                
+                var myThread = new System.Threading.Thread(() => getFiles(folderBrowserDialog1.SelectedPath));
+                myThread.Start();
+            }
+
+        }     
+
+        private void getFiles(string path)
+        {
+            lstFiles = new List<string>();
+
+            DirectoryInfo d = new DirectoryInfo(path); 
+
+            FileInfo[] Files = d.GetFiles("*.wav");
+
+            foreach (FileInfo file in Files)
+            {
+                lstFiles.Add(file.FullName);
+            }
+
+            dataGridView1.InvokeEx(y => y.DataSource = lstFiles.Select(x => new { Value = x }).ToList());
+
+        }
+
+        private void cmdPlay_Click(object sender, EventArgs e)
+        {
+
+            Random rnd = new Random();
+            int r = rnd.Next(lstFiles.Count);
+            string sFile = lstFiles[r];
+            var myThread = new System.Threading.Thread(() => PlayMusic(sFile));
+            myThread.Start();
+        }
+
+        private void PlayMusic(string file)
+        {
+            lblPlayingSong.InvokeEx(x => x.Text = "Playing: " + file);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(file);
+            player.Play();
+        }
+    }
+
+    static class ISynchronizeInvokeExtensions
+    {
+        public static void InvokeEx<T>(this T @this, Action<T> action) where T : ISynchronizeInvoke
+        {
+            try
+            {
+                if (@this.InvokeRequired)
+                {
+                    @this.Invoke(action, new object[] { @this });
+                }
+                else
+                {
+                    action(@this);
+                }
+            }
+            catch (Exception ex) { System.Diagnostics.Debug.WriteLine(ex.Message); }
+        }
+    }
+}
